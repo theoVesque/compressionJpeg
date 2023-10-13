b8aae38de90ebc831d7c7dca52e5a264d3e45860
khalil <khalil.bouchana@uphf.fr>
2023-10-13 08:19:08 +0200

    controller qui permet de décomposer une image JPEG

---
+++ compressionJpeg/listings/utils/PictureFileController.py
@@ -0,0 +1,44 @@
+class PictureFileController:
+    def __init__(self,pictureFile:str):
+        if(PictureFileController.isJpegFile(pictureFile)
+           or PictureFileController.isBitmapFile(pictureFile)
+           or PictureFileController.isPngFile(pictureFile)):
+            self.pictureFile = pictureFile
+            self.pictureFormat = pictureFile.split(".")[-1]
+        else:
+            raise Exception("File format of your picture is not valid. Only PNG, JPEG or BMP format are accepted !")
+
+    @staticmethod
+    def isJpegFile(fileName:str)->bool:
+        if(fileName.lower().split(".")[-1] in ["jpeg","jpe","jpg","jfif"]):
+            return True
+        return False
+    
+    @staticmethod
+    def isBitmapFile(fileName:str)->bool:
+        if(fileName.lower().split(".")[-1] == "bmp"):
+            return True
+        return False
+    
+    @staticmethod
+    def isPngFile(fileName:str)->bool:
+        if(fileName.lower().split(".")[-1] == "png"):
+            return True
+        return False
+    
+    def createBitmapBinaryFile(self):
+        with open(self.pictureFile,"rb") as readedFile:
+            content = readedFile.readlines()
+            writedFile = self.pictureFile.split(".")[0]+"_binaries.tx"
+            with open(writedFile,"w") as wf:
+                for line in content:
+                    wf.write(line.hex())
+
+            
+
+if __name__ == '__main__':
+    pictureC = PictureFileController("compressionJpeg/listings/static/pictures/bitmap_picture.jpg")
+    pictureC.createBitmapBinaryFile()
+        
+
+
